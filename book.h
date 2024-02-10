ea5afcc02110bdc33a1343e6407c33aec0f23572
Breeze Pickford <bpickfor@usc.edu>
2024-02-09 16:41:59 -0800

    fixed typos in product derived classes

--- book.h
+++ book.h
@@ -13,18 +13,18 @@ class Book : public Product
 public:
     // constructor
     Book(const std::string category, const std::string name, double price, int qty,
-         const std::string isbn, const std::string author)
+         const std::string isbn, const std::string author);
         // deconstructor
         ~Book();
 
     // polymorphism w/ the overriding virtual stuff from product parent
     std::set<std::string> keywords() const override;
-    stdstring displayString() const override;
+    std::string displayString() const override;
     void dump(std::ostream &os) const override;
 
 protected:
     // unique book stuff
     std::string isbn_;
     std::string author_;
-}
+};
 #endif
